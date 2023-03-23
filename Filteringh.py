26214247cf83f3641a706495366867817901f7e4
sardar-filtering <126225116+sardar-filtering@users.noreply.github.com>
2023-03-23 20:44:40 +0330

    Create Filteringh.py

---
+++ Filteringh.py
@@ -0,0 +1,203 @@
+# PAYAMBAR DYSON ☘️ 
+print("ILIYA DYSON")
+#this is bug !                                            # slm                                                           #import library's
+import os
+# auto install libs
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing libs
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# code colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+#date()
+x = str(datetime.datetime.now())
+
+print("started " + (x))
+
+print(f"{blue} ")
+print ("         Script By")
+print (Fore.YELLOW + "")
+x = """ ILIYA Dayson bot running....
+----DYSON.--------------------------DYSON-----DYSON---------------------------DYSON-----DYSON---DYSON----------DYSON----------DYSON---   DYSON-----DYSON-----DYSON----DYSON-----DYSON----DYSON----DYSON---DYSON--------DYSON----!!!!!--------DYSON----DYSON----DYSON---(ILIYA DAYSON)--------DYSON--------OSTOREH FILTERING= IM PAYAMBAR DYSON !!!!!!!
+
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+#⁶6⁶
+print(f"{red} ")
+d = """
+
+IM : ILIYA DYSON 😂😐💔🥀
+Shad Bashin
+
+                            DYSON
+         GOD FILTERING RUBIKA ILIYA DYSON
+,-.
+               _,---.          /'|\          __,---,___
+            ,-'    \    -.____,-'  |  -.____,-'    //    -.
+          ,'        |           ~'\     /~           |        .          /      ___//              . ,'          ,  , \ ___      \
+        |    ,-'   -.__   _         |        ,    __,-'   -.    |
+        |   /          /\_     .    |    ,      _/\          \   |
+        \  |           \ \-.___ \   |   / ___,-'/ /           |  /
+         \  \           | ._   \  |  //'   _,' |            /  /
+          -.\         /'  _ ---'' , . ``---' _  \         /,-'
+             `       /     \    ,='/ \=.    /     \       ''
+                     |__   /|\_,--.,-.--,--._/|\   __|
+                     /  ./  \\ |  |  | /,//'   \   \     ILIYA  ~> DYSON
+                    /   /     ||--+--|--+-/-|     \   \
+                   |   |     /'\_\_\ | /_/_/\     |   |
+                    \   \__, \_     ~'     _/ .__/   /
+                     -._,-'   -._______,-'   -._,-'
+
+                    CODE FILTERI RUBIKA
+             ILIYA DYSON 💗
+          WEBLOG GHAVANIN ~> rules-majazi.Blogfa.com Ostoreh Filter/\/\/\
+
+
+Filtering RUBIKA running //////////////////////////////////////////////////////////////////////////.......
+"""
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+# TARA *_*
+#my love
+#          ▄▀▄   I LOVE YOU▄▀▄
+#         All Chaker Ton ILIYA 
+#     
+#   
+
+time.sleep(4.3)
+print(f"{green} ")
+print ("ok ILIYA Dyson . ")
+print (Fore.YELLOW + "")
+time.sleep(1.7)
+print ("                       ID RUBIKA @Makerby_dyson IM ILIYA")
+print ("                         ID telegram @makerby_dayson")
+print ("                          WebLog ghavanin : rules-majazi.blogfa.com ")
+print(f"{blue} ")
+time.sleep(1.0)
+print ("__________")
+time.sleep(0.7)
+print ("#I_________")
+time.sleep(0.6)
+print ("##L________")
+time.sleep(0.5)
+print ("###I_______")
+time.sleep(0.4)
+print ("####Y______")
+time.sleep(0.3)
+print ("#####A_____")
+time.sleep(0.2)
+print ("######D____")
+time.sleep(0.1)
+print ("#######Y___")
+time.sleep(0.5)
+print ("########S__")
+time.sleep(0.1)
+print ("#########O_")
+time.sleep(0.5)
+print ("##########0")
+print ("")
+# vared kardane addad baraye gereftan kod
+up = input("Code Filter Group (3) && Code Filter Account (4) > > > ")
+print(f"{pink} ")
+time.sleep(3.0)
+print(""" in the name of God 
+_____________
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Loading . . . . iliya dyson """)
+
+print(f"{red} ")
+
+
+
+if up == "2":
+
+        print ('code for Channel ≠>' + '%s%s %78ilter.%73 %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+if up != "2":
+        print("error - lotfan dorost vared konid")
+
+
+if up == "3":
+        print ('code group ≠>' + '%s%s (/*!50000(4.7.9.0/f////g.h/4.3.4.6.7.9.8.5.2.5.7.9.5.2.3.6.8.0.0.7.3.1/)) %s' % (fg('white'), bg('black'), attr('reset')))
+
+
+if up != "3":
+
+        print("Code Sakhteh Shode Tavasot Dyson")
+
+
+if up == "4":
+
+        print ('code Filter account ≠>' + '%s%s (/yftt-09/80fs.i.q/)         && (utt(_0/fg0/K/G0))   .   &&    .     (l98"=5dVBV_I>aBfn/)             &&        (/3.2.9.&.dst2ee76RbZP0.py/i.q.w.6.0.3.8//)             &&          (f1726b6cc718-67//80)            && gmjjxoo28_6.8.9/)    && .      (/yftt_18//(#b3#0398_-/)   .     && (fἶl_Γυჩἶκმ.yf7715k.8.9.2.7/) .    && (9.1.3.8/filter_rubika/h3.&.5.8.9.3.&.€l.utt-09.¥i.c)            .&&      (http://uyftt16k.i.q.w.t.k0f.3.4.1/3.8.9.5////)        %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
+
+if up != "4":
+        print("Lider Dyson")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""
+Kanal haye rubika ILIYA DYSON =
+
+@Username_Dyson + @Username_Dyson
+
+@Bi3eday + @Bi3eday
+ """)
+print ("""
+link Tonel rubikam DYSON==>>>
+https://rubika.ir/joing/DJDBICIJ0TJBMUIRICYCBUPUQGLRLNNL""")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Off Mizanam Az Rubika Im ILIYA DYSON")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print (' In Ro Befrest = @-3.4.6 Bad Blake Kon' + '%s%s  /  %s' % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print ("""code ro ( 3 ) bar bezanin to gozaresha
+baraye account Lotfan Shakh Bazi Dar Naiarid Tanks""")
+print(" ")
+print(" ")
+print(" ")
+time.sleep(2.0)
+os.system("cd Filtering.py")
+os.system("rm -rf /data/data/com.termux/files/home/sardar.filtering.infect && git clone https://github.com/sardar-filtering/sardar.filtering.infect")
+print ("time end :")
+print (Fore.WHITE + "")
+date()
